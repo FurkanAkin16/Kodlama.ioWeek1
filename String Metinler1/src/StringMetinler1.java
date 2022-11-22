45ba7b2c673df3078df1c6b15378678407140a5c
Furkan <118662756+FurkanAkin16@users.noreply.github.com>
2022-11-22 15:36:01 +0300

    Add files via upload

---
+++ String Metinler1/src/StringMetinler1.java
@@ -0,0 +1,17 @@
+
+public class StringMetinler1 {
+
+	public static void main(String[] args) {
+		String üstBaslık =  "Erikli";
+		String ortaBaslık = "Damla";
+		String altBaslık =  "Uludağ";
+		
+		System.out.println(altBaslık);
+		System.out.println(ortaBaslık);
+		System.out.println(üstBaslık);
+		System.out.println(üstBaslık);
+		
+
+	}
+
+}
