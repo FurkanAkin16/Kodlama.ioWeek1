5d70afbb71f36df7868d6014b92ce1c90b893d5c
Furkan <118662756+FurkanAkin16@users.noreply.github.com>
2022-11-22 15:31:57 +0300

    Add files via upload

---
+++ ArrayDemo1/src/For Listeleme1/src/ForListeleme1.java
@@ -0,0 +1,16 @@
+
+public class ForListeleme1 {
+
+	public static void main(String[] args) {
+		//array
+		String[] ürünler  = {"elma", "armut", "havuç",};
+		
+		for (int i = 0; i < ürünler.length; i++) {
+			System.out.println(ürünler[i]);
+			
+			
+		}
+
+	}
+
+}
