ee3ce75820036445cf15937510ef3a43cc692ce6
Furkan <118662756+FurkanAkin16@users.noreply.github.com>
2022-11-22 15:32:57 +0300

    Add files via upload

---
+++ LoopDemo/src/LoopDemo1.java
@@ -0,0 +1,32 @@
+
+public class LoopDemo1 {
+
+	public static void main(String[] args) {
+		
+		
+		for (int i=1;i<=10;i++) {
+			System.out.println(i);
+		}
+		System.out.println("For Döngüsü Bitti");
+		
+		int i = 1;
+		//while
+		while (i<10) {
+			System.out.println(i);
+			i++;
+		
+	}
+		System.out.println("While Döngüsü Bitti");
+		
+		int j=100;
+		do {
+					System.out.println(j);
+					j+=2;
+					}
+					
+		 while (j<10); 
+		System.out.println("Do-While Döngüsü Bitti");
+	
+	  
+	}
+}
