6ac9bff6e0efa5b938411a5a6e4faa4d1c940bf8
Furkan <118662756+FurkanAkin16@users.noreply.github.com>
2022-11-22 15:31:25 +0300

    Add files via upload

---
+++ ArrayDemo1/src/ArrayDemo1.java
@@ -0,0 +1,14 @@
+
+public class ArrayDemo1 {
+
+	public static void main(String[] args) {
+
+		String[] öğrenciler = { "Ali Eymen", "Beyza", "Furkan", "Seçkin" };
+
+		for (int i = 0; i < öğrenciler.length; i++) {
+			System.out.println(öğrenciler[i]);
+		}
+
+	}
+
+}
