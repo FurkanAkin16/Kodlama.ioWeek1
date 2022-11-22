14f075fcca2cd0b5e2a9c45281c465ea7c01ca2b
Furkan <118662756+FurkanAkin16@users.noreply.github.com>
2022-11-22 15:36:52 +0300

    Add files via upload

---
+++ WhileLoop1/src/WhileLoop1.java
@@ -0,0 +1,18 @@
+
+public class WhileLoop1 {
+
+	public static void main(String[] args) {
+		
+
+		int i=1;
+		//while
+		while (i<10) {
+			System.out.println(i);
+			i++;
+
+		}
+		System.out.println("While Döngüsü Bitti");
+		
+	}
+
+}
